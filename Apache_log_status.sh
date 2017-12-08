3719eebf98329181059b1073578e299fe13ebe26
84795949 <84795949@qq.com>
2017-12-08 11:24:38 +0800

    修改了默认日志位置

--- Apache_log_status.sh
+++ Apache_log_status.sh
@@ -13,18 +13,18 @@ download(){
 	else
 		exit
 	fi
-	echo 下载为本地$LOG
+	echo 下载为本地: $LOG
 }
 
 select_log(){
-	read -p "请输入需要截取的日志所在服务器(pcw|cad): " Server	
+	read -p "请输入需要截取的日志文件(绝对路径)：" LOG
 	read -p "请输入选取日志的开始时间(yyyy:hh:mm): " StartDate
 	Start=`cat $LOG |grep ${StartDate}|head -n 1|awk '{print $4$5}'|tr -cd "[0-9]"`
 	read -p "请输入选取日志的结束时间(yyyy:hh:mm): " EndDate
 	End=`cat $LOG |grep ${EndDate}|tail -n 1|awk '{print $4$5}'|tr -cd "[0-9]"`
 	sed -n "/$StartDate:${Start:11:2}/,/$EndDate:${End:11:2}/p" $LOG &> $DIR/${Server}_apache_${StartDate:5:5}-${EndDate:5:5}.log
-	echo 截取日志另存为: $DIR/${Server}_apache_${StartDate:5:5}-${EndDate:5:5}.log
-	echo $DIR/${Server}_apache_${StartDate:5:5}-${EndDate:5:5}.log > $DIR/.log
+	echo 截取日志另存为: $DIR/apache_${StartDate:5:5}-${EndDate:5:5}.log
+	echo $DIR/apache_${StartDate:5:5}-${EndDate:5:5}.log > $DIR/.log
 }
 
 
